bbd1bc5e844d96adbefe6b491d8f100a5007ddea
Alexander Pliskin <a.pliskin@tinkoff.ru>
2021-08-18 22:19:42 +0300

    Добавили контроллер

--- src/main/java/ru/tinkoff/sirius/web/controller/UserController.java
+++ src/main/java/ru/tinkoff/sirius/web/controller/UserController.java
@@ -1,9 +1,10 @@
 package ru.tinkoff.sirius.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.tinkoff.sirius.web.model.User;
 import ru.tinkoff.sirius.web.service.UserService;
 
 @RequestMapping("/users")
@@ -13,5 +14,25 @@ import ru.tinkoff.sirius.web.service.UserService;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getUser(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User createUser(/*@Valid*/ @RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User updateUser(/*@Valid*/ @RequestBody User user) {
+        return userService.update(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+    }
 
 }
