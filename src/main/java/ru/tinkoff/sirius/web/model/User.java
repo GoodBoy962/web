63dbb61009b4481958b293c14c27dc95b322b148
Alexander Pliskin <a.pliskin@tinkoff.ru>
2021-08-18 22:44:39 +0300

    Выключили ExceptionHandler

--- src/main/java/ru/tinkoff/sirius/web/model/User.java
+++ src/main/java/ru/tinkoff/sirius/web/model/User.java
@@ -1,23 +1,28 @@
 package ru.tinkoff.sirius.web.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 /**
  * Сущность пользователя
  */
-@Data
+@Getter
+@Setter
 @Schema(description = "Пользователь")
 public class User {
 
     private Long id;
     @NotBlank
     private String login;
+    @Schema(description = "Фамилия Имя Отчество пользователя", required = true)
     private String fio;
     //    @ValidPhoneNumber
+    @Schema(description = "Телефонный номер", required = true)
     private String phoneNumber;
+    @Schema(description = "Аккаунт пользователя")
     private String account;
 
 }
